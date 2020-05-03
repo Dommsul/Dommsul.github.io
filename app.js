d66d9f6e0e2b74c8acfd39af33cc1132f1d9173a
Dommsul <56399546+Dommsul@users.noreply.github.com>
2020-05-03 19:59:47 +0100

    Add files via upload
    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+function changePic() {
+    document.getElementById("profilePic").src = "images/profilepic2.jpg";
+  }
+  
+  function normalPic() {
+    document.getElementById("profilePic") .src = "images/finalpicport.jpg";
+  }
+
